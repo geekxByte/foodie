4fc88ec7894aee1ed31fb6546ca459ffb6a09b8a
geekxByte <115074475+geekxByte@users.noreply.github.com>
2023-07-07 00:51:34 -0700

    Add files via upload

---
+++ foodie/src/components/Page/Resturant.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './style.css';
+import Menu from './MenuApi';
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+const uniqueList = [
+    ...new Set(
+        Menu.map((curEle) => {
+            return curEle.category;
+        })
+    ),
+    "All",
+]
+
+const Resturant = () => {
+    const [menuData,setMenuData]=useState(Menu);
+    const [menuList, setMenuList]=useState(uniqueList);
+
+    const filterItem = (category) => {
+        if(category==='All'){
+            setMenuData(Menu);
+            return;
+        }
+        const updatedItems = Menu.filter((curEle) => {
+            return curEle.category === category;
+        });
+        setMenuData(updatedItems);
+    };
+
+  return (
+    <>
+        <Navbar filterItem={filterItem} menuList={menuList}/>
+        <MenuCard menuData={menuData}/>
+    </>
+  );
+};
+
+export default Resturant;
